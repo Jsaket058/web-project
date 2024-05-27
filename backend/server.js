e5d374bca90139d43980716daf1f3a719709037b
Saket Jain <jsaket058@gmail.com>
2024-05-28 01:34:57 +0530

    Fifth

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,5 @@ const PORT= process.env.PORT || 5000;
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`);
 });
+
+//mongodb+srv://jsaket058:<password>@saketcluster.hdor5sy.mongodb.net/?retryWrites=true&w=majority&appName=SaketCluster
